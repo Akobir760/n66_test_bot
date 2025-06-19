c08e151e6ded805378015aac5c2d0a3479c222d5
Akobir <akobirortiqov40@gmail.com>
2025-06-19 16:06:47 +0500

    finish

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 from asyncio import run
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+user_main_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Menyu")],
+        [KeyboardButton(text="Mening buyurtmalarim")],
+        [KeyboardButton(text="Savat"), KeyboardButton(text="Aloqa")],
+        [KeyboardButton(text="Xabar yuborish"), KeyboardButton(text="Sozlamalar")],
+    ],
+    resize_keyboard=True,
+    is_persistent=True
+)
+
+menu_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Setlar")],
+        [KeyboardButton(text="Lavash"), KeyboardButton(text="Shaurma")],
+        [KeyboardButton(text="Burger"), KeyboardButton(text="Hot-Dog")],
+        [KeyboardButton(text="Ichimliklar")],
+        [KeyboardButton(text="Orqaga")],
+    ],
+    resize_keyboard=True,
+    is_persistent=True
+)
 
 async def start_handler(message: types.Message, bot: Bot):
-    print(message.from_user.full_name)
-    await bot.send_message(chat_id=message.chat.id, text = "Salom")
-    await message.answer(text=message.text)
+    await bot.send_message(chat_id=message.chat.id, text = "🛒 Asosiy Menyu\n Marhamat buyurtma berishingiz mumkin!", reply_markup=user_main_keyboard)
 
+async def menu_handler(message: types.Message):
+    text = "Tanlang:"
+    await message.answer(text=text, reply_markup=menu_keyboard)
+
+async def my_orders_handler(message: types.Message):
+    text = "Asosiy menu bosildi"
+    await message.answer(text=text)
 
 async def main():
     bot = Bot(token="8099267684:AAEIOZGBnGK5yNy1f1IMsuu9SzH5_-BqNKA")
     dp = Dispatcher()
     dp.message.register(start_handler, Command('start'))
+    dp.message.register(menu_handler, F.text == "Menyu")
+    dp.message.register(start_handler, F.text == "Orqaga")
+    dp.message.register(my_orders_handler, F.text == "Savat")
+    dp.message.register(my_orders_handler, F.text == "Mening buyurtmalarim")
+    dp.message.register(my_orders_handler, F.text == "Aloqa")
+    dp.message.register(my_orders_handler, F.text == "Xabar yuborish")
+    dp.message.register(my_orders_handler, F.text == "Sozlamalar")
 
     await dp.start_polling(bot, polling_timeout=0)
 
