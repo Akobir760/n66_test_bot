7908ebd5b3e6aa9a31639a5eac404d02e6d634a6
Akobir <akobirortiqov40@gmail.com>
2025-06-21 18:02:41 +0500

    states

--- routers/common.py
+++ routers/common.py
@@ -1,9 +1,12 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from states.user import Register
 
 router = Router()
 
 @router.message(Command('start'))
-async def start_handler(message: types.Message):
+async def start_handler(message: types.Message, state: FSMContext):
     text = "Assalomu alaykum"
     await message.answer(text=text)
+    await state.set_state(Register.language)
--- routers/register.py
+++ routers/register.py
@@ -1,3 +1,4 @@
 from aiogram import Router
 
 router = Router()
+
