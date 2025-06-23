e3f8eaf2cd3af619324eb0acdb8d4885273592bb
Akobir <akobirortiqov40@gmail.com>
2025-06-23 19:22:12 +0500

    fix

--- core/config.py
+++ core/config.py
@@ -1,16 +1,20 @@
 import os
 
-import psycopg2
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(
+    dotenv_path=".env"
+)
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
+
 TOKEN = os.getenv("TOKEN")
+DEVELOPER = 844817222
+ADMINS = []
 
 DB_CONFIG = {
     "database": DB_NAME,
@@ -19,8 +23,3 @@ DB_CONFIG = {
     "host": DB_HOST,
     "password": DB_PASS
 }
-def get_connection():
-    try:
-        return psycopg2.connect(**DB_CONFIG)
-    except psycopg2.Error as e:
-        print(e)
